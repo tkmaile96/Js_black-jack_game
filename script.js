2ab516bdcc70ba88e8664ed9057bbc230df3c405
Teekay <tkmaile96@gmail.com>
2024-07-23 08:44:34 +0200

    added logic

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ let secondCard = 10;
 
 let sum = firstCard + secondCard;
 
-
+if (sum < 21) {
+    console.log("Do you want to draw a new card?")
+}if else (sum === 21) {
+    console.log ("Wohoo! You've got Blackjack")
+}else {
+    console.log ("You're out of the game")
+}
  
